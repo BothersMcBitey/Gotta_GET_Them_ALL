9120946c239418cf3a691ba132daf06b4d764ddb
Harvinder Tank <harvitank@hotmail.co.uk>
2025-04-23 15:37:03 +0100

    Assign pokemon or choose pokemon

--- random_pokeman.py
+++ random_pokeman.py
@@ -11,6 +11,22 @@ pokemon_list = json.loads(response.text)['results']
 for pokemon in pokemon_list:
     print(pokemon['name'])
 
+
 cpu_choice = random.choice(pokemon_list)['name']
+
+# Loop until user gives a valid input
+while True:
+    user_choice = input("Would you like a random Pokémon (R) or choose (C)? ").upper()
+
+    if user_choice == 'R':
+        user_pokemon = random.choice(pokemon_list)['name']
+        print(f"User chose: {user_pokemon}")
+        break
+    elif user_choice == 'C':
+        user_pokemon = input("Enter your Pokémon: ").lower()
+        print(f"User chose: {user_pokemon}")
+        break
+    else:
+        print("Invalid input, please write R or C.")
 
 print(f"CPU chose: {cpu_choice}")
